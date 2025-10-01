297f743bbcdd2140f4f475c78fa8fffc57e27a3c
ayoub <Ayoub.ELAZZOUZI@UM6P.ma>
2025-10-01 15:59:42 +0100

    1st and 2nd exercice

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -4,5 +4,33 @@ public class CourseNumbersArray {
     public static void main(String[] args) {
         int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
 
+        //Adding a new course
+        int[] updatedCourses = new int[registeredCourses.lenght + 1];
+        for (int i = 0 ; i < registeredCourses.lengh ; i++){
+            updatedCourses[i] = registeredCourses[i];
+        }
+        updatedCourses[registeredCourses] = 3150 // the newly added course appened on the right;
+
+        //Printing the content of updatedCourses
+        for (int course : updatedCourses){
+            System.out.println(course);
+        };
+
+        //Checking existance of a course
+        int course = 1020;
+        boolean exists = false;
+        for (int i : updatedCourses) {
+            if (i == course) {
+                exists = true;
+                break;
+            }
+        }
+
+        if ( exists){
+            System.out.println("The course " + course + " does exist");
+        }
+        else{
+            System.out.println("The course "+ course + " does not exist");
+        }
     }
 }
